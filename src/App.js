a126f76d4d635441f61215d71aa21d828814ad7e
jeremy-adamson <jadamson.physics@gmail.com>
2023-02-04 17:39:36 -0800

    trying lab7 again

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import React from "react";
 import Header from "./Header.js";
 import Main from "./Main.js";
 import Footer from "./Footer.js";
-import axios from 'axios';
-import { render } from '@testing-library/react';
 
 class App extends React.Component {
   render() {
--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { ListGroup } from "react-bootstrap";
 
 class Weather extends React.Component{
     render(){
